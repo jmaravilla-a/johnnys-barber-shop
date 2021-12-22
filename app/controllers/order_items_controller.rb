c712fc96e38a426690e117960ae004bec1852eff
Javier Maravilla <javier.maravilla@outlook.com>
2021-12-22 17:01:43 -0500

    Started adding custom actions on order_items controller.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,22 @@
 class OrderItemsController < ApplicationController
+
+    def create 
+        # Check to see if an order exists with the paid boolean being false
+        unpaidOrder = @current_user.Order.where(paid: false)
+        # Create an instance of order_items for each item sent 
+        if unpaidOrder
+            
+            
+        # If such order exists, skip creating an order. Else:
+        # Create an instance of order
+        # Then,
+        # Create an instance of order_items for each item sent 
+        # 
+    end
+
+    private
+
+    def item_params
+        
+    end
 end
