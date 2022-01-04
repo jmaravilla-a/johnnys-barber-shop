2eac45ea2431aee7fba3473ea6e42abdae5d51f8
Javier Maravilla <javier.maravilla@outlook.com>
2022-01-04 17:10:11 -0500

    MVP METgit add .

--- app/controllers/checkout_sessions_controller.rb
+++ app/controllers/checkout_sessions_controller.rb
@@ -4,22 +4,36 @@ class CheckoutSessionsController < ApplicationController
     Stripe.api_key = ENV["SECRET_API_KEY"]
     
     def create
-        price_ids = params["price_id"]
-        price_ids_array = price_ids.map{|price_id| {price: price_id, quantity: 1}}
-        
-        stripe_session = Stripe::Checkout::Session.create({
-            line_items: price_ids_array,
-            mode: 'payment',
-              success_url: 'http://localhost:3001/',
-              cancel_url: 'http://localhost:3001/'
-              })
-                # redirect_to stripe_session.url
-                url = stripe_session.url
-            #   byebug
+        price_ids = price_id_params[:price_id]
+        if price_ids.length > 0
+            
+            price_ids_array = price_ids.map{|price_id| {price: price_id, quantity: 1}}
+            
+            stripe_session = Stripe::Checkout::Session.create({
+                line_items: price_ids_array,
+                mode: 'payment',
+                success_url: 'http://localhost:3001/',
+                cancel_url: 'http://localhost:3001/'
+                });
 
-        render json: url, status: :ok
+
+            url = [stripe_session.url]
+
+            @current_user.orders.where(paid: false).update(paid: true)
+            
+            render json: url, status: :ok
+
+        else
+            render json: {message: "Please add items to your cart"}
+        end
+        
               
     end
 
+    private
+
+    def price_id_params
+        params.permit(:price_id => [])
+    end
 
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,15 +1,14 @@
 class OrderItemsController < ApplicationController
 
     def create 
-        # byebug
         # Check to see if an order exists with the paid boolean being false
         order = @current_user.orders.where(paid: false)
         # If unpaid order exists, create an instance of order_items for the item sent 
-        if order
-            @new_item = OrderItem.new(item_params)
-            @new_item.order_id = order.first.id
-            @new_item.save
-            render json: {message: "Added to cart!"}, status: :created
+        if order.length > 0
+            new_item = OrderItem.new(item_params)
+            new_item.order_id = order.first.id
+            new_item.save
+            render json: {message: "#{new_item[:item_name]} added to cart!"}, status: :created
         else
             new_order = Order.create(date: DateTime.now.to_date, user_id: @current_user.id)
             new_item = OrderItem.new(item_params)
