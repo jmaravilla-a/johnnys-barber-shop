3f06dcffbc69cd38cebd9b5cd22c22d4668e13b5
Javier Maravilla <javier.maravilla@outlook.com>
2022-01-06 03:03:25 -0500

    Code freeze.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
     skip_before_action :authorize_user, only: [:create]
 
     def create
+
       user = User.find_by(email: params[:email])
       if user&.authenticate(params[:password])
         session[:id] = user.id 
